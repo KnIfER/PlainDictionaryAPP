0bdbd901f991b23e603af43ddfbec4a9779ab19f
KnIfER <knziha@gmail.com>
2019-06-07 05:01:48 +0800

    2019-06-07

--- src/main/java/androidx/appcompat/app/AlertDialog.java
+++ src/main/java/androidx/appcompat/app/AlertDialog.java
@@ -783,7 +783,6 @@ public class AlertDialog extends AppCompatDialog implements DialogInterface {
                 final OnClickListener listener) {
             P.mItems = P.mContext.getResources().getTextArray(itemsId);
             P.mOnClickListener = listener;
-
             P.mCheckedItem = checkedItem;
             P.mIsSingleChoice = true;
             return this;
