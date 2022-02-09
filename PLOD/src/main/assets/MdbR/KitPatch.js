9aa91a61f4dea0bd0c266249dce4cb6e98ff5086
KnIfER <302772670@qq.com>
2022-02-09 11:52:59 +0800

    做设置

---
+++ PLOD/src/main/assets/MdbR/KitPatch.js
@@ -0,0 +1,32 @@
+
+if (typeof String.prototype.startsWith != 'function') {  
+    String.prototype.startsWith = function (prefix){  
+        return this.slice(0, prefix.length) === prefix;  
+    };  
+} 
+if (typeof String.prototype.endsWith != 'function') {  
+    String.prototype.endsWith = function (suffix){  
+        return this.slice(-suffix.length) === suffix;  
+    };  
+} 
+if (typeof Object.assign != 'function') {
+    Object.assign = function (target, varArgs) {
+        'use strict';
+        if (target == null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+        var to = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+            var nextSource = arguments[index];
+
+            if (nextSource != null) {
+                for (var nextKey in nextSource) {
+                    if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+        }
+        return to;
+    };
+}
---
+++ PLOD/src/main/assets/MdbR/trans.js
@@ -0,0 +1,37 @@
+var trans={
+     "start" : ""
+    ,"foldAll" : "折叠全部"
+    ,"unfoldAll" : "展开全部"
+    ,"unFoldCurrent" : "只展开当前页面"
+    ,"saveCurrent" : "保存当前页面"
+    ,"showAbout" : "关于…"
+
+    ,"copyText" : "复制文本"
+    ,"inPageSearch" : "发送到页内搜索"
+    ,"anchorPageSearch" : "锚点页内搜索"
+    ,"browserSearch" : "浏览器搜索"
+    ,"mirrorSch" : "镜像搜索"
+    ,"goBack" : "后退"
+    ,"goForward" : "前进"
+    ,"focusContent" : "折叠当前页面"
+    ,"focusContent0" : "上一个"
+    ,"focusContent1" : "下一个"
+
+    ,"sendAdvanced" : "发送到高级搜索"
+
+    ,"reloadDict" : "重新加载词典"
+    ,"openFolder" : "打开所在文件夹"
+
+    ,"focusMode1" : "专注模式1（折叠）"
+    ,"focusMode2" : "专注模式2（限高）"
+    ,"autoScroll1" : "滚动快"
+    ,"autoScroll2" : "滚动慢"
+    ,"autoScroll3" : "滚动停"
+    ,"toTop" : "回到顶部"
+
+    ,"pageUpIcon" : "▲"
+    ,"pageDownIcon" : "▼"
+    ,"expandText" : "展开全文"
+    ,"foldText" : "收起全文"
+    ,"settings" : "页面设置"
+};
